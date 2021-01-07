cbed508c98455d6349b9309b6246f765f66cf0f0
Shelsy Elaine <elainesbarrera@gmail.com>
2021-01-07 12:49:44 -0800

    ADDED: console.logging connection errors and success

--- config/connection.js
+++ config/connection.js
@@ -8,4 +8,12 @@ const connection = mysql.createConnection({
   port: 3306,
 });
 
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
 module.exports = connection;
