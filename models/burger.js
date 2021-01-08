ad829c9c425fe0d94cd5649a9cc4af60cff0e9d1
Shelsy Elaine <elainesbarrera@gmail.com>
2021-01-08 15:59:11 -0800

    ADDED: layout main html

--- models/burger.js
+++ models/burger.js
@@ -3,4 +3,3 @@ const orm = require("/config/orm");
 
 // create and export the code that will call the ORM functions using burger specific input for the ORM.
 
-orm.selectAll("burgers");
--- server.js
+++ server.js
@@ -1,16 +1,1 @@
 const express = require("express");
-
-const orm = require("./config/orm");
-
-// create and export the code that will call the ORM functions using burger specific input for the ORM.
-
-// orm.selectAll("burgers");
-// orm.insertOne(
-//   "burgers",
-//   "burger_name",
-//   "devoured",
-//   "Chilly Cheese Burger",
-//   true
-// );
-
-// orm.updateOne("burgers", "burger_name", "Double Cheese Burger", "id", 3);
