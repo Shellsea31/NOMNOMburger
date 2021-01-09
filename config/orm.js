9b393383581d589a9b835edaab063918740c3a9c
Shelsy Elaine <elainesbarrera@gmail.com>
2021-01-08 16:44:13 -0800

    ADDED: callback to all orm functions

--- config/orm.js
+++ config/orm.js
@@ -2,29 +2,29 @@ const connection = require("./connection");
 
 // create the methods that will execute the necessary MySQL commands in the controllers.
 const orm = {
-  selectAll(table) {
+  selectAll(table, cb) {
     connection.query("SELECT * FROM ??", [table], (err, data) => {
       if (err) throw err;
-      console.log(data);
+      cb(data);
     });
   },
-  insertOne(table, param1, param2, param1Val, param2Val) {
+  insertOne(table, param1, param2, param1Val, param2Val, cb) {
     connection.query(
       "INSERT INTO ?? (??, ??) VALUES (?, ?)",
       [table, param1, param2, param1Val, param2Val],
-      (err, data) => {
+      (err, result) => {
         if (err) throw err;
-        console.log(data);
+        cb(result);
       }
     );
   },
-  updateOne(table, column, colVal, condition, condVal) {
+  updateOne(table, column, colVal, condition, condVal, cb) {
     connection.query(
       "UPDATE ?? SET ?? = ? WHERE ?? = ?",
       [table, column, colVal, condition, condVal],
-      (err, data) => {
+      (err, result) => {
         if (err) throw err;
-        console.log(data);
+        cb(result);
       }
     );
   },
