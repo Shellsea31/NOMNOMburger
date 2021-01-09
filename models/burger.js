2ca252be31065d82b7d5865621ea370423435b14
Shelsy Elaine <elainesbarrera@gmail.com>
2021-01-08 17:20:53 -0800

    UPDATE: orm functions working with models

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,24 @@
-const { selectAll } = require("../config/orm");
-const orm = require("/config/orm");
+const { insertOne } = require("../config/orm");
+const orm = require("../config/orm");
 
 // create and export the code that will call the ORM functions using burger specific input for the ORM.
+const burger = {
+  selectAll() {
+    orm.selectAll("burgers", (result) => console.log(result));
+  },
+  insertOne(param1Val, param2Val) {
+    orm.insertOne(
+      "burgers",
+      "burger_name",
+      "devoured",
+      param1Val,
+      param2Val,
+      (result) => console.log(result.affectedRows)
+    );
+  },
+};
 
+burger.selectAll();
+burger.insertOne("Fried Burger", false)
+
+module.exports = burger;
