389f243abab8f636e8a9977c62f44873f77e3acf
Shelsy Elaine <elainesbarrera@gmail.com>
2021-01-09 08:40:10 -0800

    UPDATE: setup server port, app use and engine

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function () {
+  console.log(`Listening on: http://localhost:${PORT}`);
+});
