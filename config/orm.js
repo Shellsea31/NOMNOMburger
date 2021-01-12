e54eb408fb13bbf433ac791816dd8aa199b41bc4
Shelsy Elaine <elainesbarrera@gmail.com>
2021-01-12 13:10:06 -0800

    reverted most changes

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-const connection = require("./connection.js");
+const connection = require("./connection");
 
 // create the methods that will execute the necessary MySQL commands in the controllers.
 const orm = {
