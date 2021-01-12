fd6eeb8e4353d11feec992d5439a051d7df80156
Shelsy Elaine <elainesbarrera@gmail.com>
2021-01-12 13:33:22 -0800

    removed uncommented

--- config/connection.js
+++ config/connection.js
@@ -12,14 +12,6 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "ShellseaFS2020!",
-//   database: "burgers_db",
-//   port: 3306,
-// });
-
 connection.connect((err) => {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
